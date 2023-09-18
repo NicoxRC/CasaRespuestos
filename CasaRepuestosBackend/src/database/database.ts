2f9295b01860f82254ea265a323ffcef8eca1d96
NicoxRC <nicorojas005@gmail.com>
2023-09-18 10:45:42 -0500

    database for production

--- CasaRepuestosBackend/src/database/database.ts
+++ CasaRepuestosBackend/src/database/database.ts
@@ -1,53 +1,53 @@
 import { Sequelize } from 'sequelize';
 import store from '../config/index.js';
 
-// export let sequelize:any = new Sequelize({
-//   database: store.DB_NAME,
-//   dialect: 'postgres',
-//   host: store.DB_HOST,
-//   port: Number(store.DB_PORT),
-//   username: store.DB_USER,
-//   password: store.DB_PASSWORD,
-//   pool: {
-//     max: 3,
-//     min: 1,
-//     idle: 10000,
-//   },
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       // Ref.: https://github.com/brianc/node-postgres/issues/2009
-//       rejectUnauthorized: false,
-//     },
-//     keepAlive: true,
-//   },
-//   ssl: true,
-// });
+export let sequelize:any = new Sequelize({
+  database: store.DB_NAME,
+  dialect: 'postgres',
+  host: store.DB_HOST,
+  port: Number(store.DB_PORT),
+  username: store.DB_USER,
+  password: store.DB_PASSWORD,
+  pool: {
+    max: 3,
+    min: 1,
+    idle: 10000,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      // Ref.: https://github.com/brianc/node-postgres/issues/2009
+      rejectUnauthorized: false,
+    },
+    keepAlive: true,
+  },
+  ssl: true,
+});
 
-export let sequelize: any =
-  process.env.NODE_ENV === 'production'
-    ? new Sequelize({
-        database: store.DB_NAME,
-        dialect: 'postgres',
-        host: store.DB_HOST,
-        port: Number(store.DB_PORT),
-        username: store.DB_USER,
-        password: store.DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${store.DB_USER}:${store.DB_PASSWORD}@${store.DB_HOST}/${store.DB_NAME}`,
-        { logging: false, native: false }
-      );
+// export let sequelize: any =
+//   process.env.NODE_ENV === 'production'
+//     ? new Sequelize({
+//         database: store.DB_NAME,
+//         dialect: 'postgres',
+//         host: store.DB_HOST,
+//         port: Number(store.DB_PORT),
+//         username: store.DB_USER,
+//         password: store.DB_PASSWORD,
+//         pool: {
+//           max: 3,
+//           min: 1,
+//           idle: 10000,
+//         },
+//         dialectOptions: {
+//           ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//           },
+//           keepAlive: true,
+//         },
+//         ssl: true,
+//       })
+//     : new Sequelize(
+//         `postgres://${store.DB_USER}:${store.DB_PASSWORD}@${store.DB_HOST}/${store.DB_NAME}`,
+//         { logging: false, native: false }
+//       );
